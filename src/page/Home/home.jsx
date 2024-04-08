1485ea28db82aefc7c0638f69d77b1a8f7385328
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-04-08 10:34:19 +0530

    food list added

--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/navBar";
 import "./style.css";
 
 function Home() {
-  const [selectedList, setSelectedList] = useState(0);
+  const [selectedList, setSelectedList] = useState(0); // Initialize with -1 to indicate no selection
   // Define an array of food items
   const foodItems = [
     "Foods",
@@ -19,6 +19,33 @@ function Home() {
     "Drinks",
     "Snacks",
   ];
+  const foodData = [
+    {
+      dishName: "Veg maggie with samosa",
+      price: "₹400",
+      img: "https://www.healthifyme.com/blog/wp-content/uploads/2021/10/All-About-The-Right-Food-Plate-Method.jpg",
+    },
+    {
+      dishName: "Veg maggie with tomoto and onion",
+      price: "₹400",
+      img: "https://www.healthifyme.com/blog/wp-content/uploads/2021/10/All-About-The-Right-Food-Plate-Method.jpg",
+    },
+    {
+      dishName: "Veg maggie",
+      price: "₹400",
+      img: "https://www.healthifyme.com/blog/wp-content/uploads/2021/10/All-About-The-Right-Food-Plate-Method.jpg",
+    },
+    {
+      dishName: "Veg maggie",
+      price: "₹400",
+      img: "https://www.healthifyme.com/blog/wp-content/uploads/2021/10/All-About-The-Right-Food-Plate-Method.jpg",
+    },
+  ];
+
+  // Function to handle food item click
+  const handleFoodItemClick = (index) => {
+    setSelectedList(index); // Set selected index
+  };
 
   return (
     <div>
@@ -40,8 +67,36 @@ function Home() {
       <div className="horizontal-scroll">
         <div className="food-list">
           {foodItems.map((item, index) => (
-            <div key={index} className="food-item">
-              {item}
+            <div
+              key={index}
+              className="food-item"
+              onClick={() => handleFoodItemClick(index)}
+            >
+              <p className={selectedList === index ? "selected" : "unselected"}>
+                {item}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="horizontal-scroll">
+        <div className="d-flex mb-5 ">
+          {foodData.map((item, index) => (
+            <div key={index} className="food-data-item">
+              <div className="">
+                <div className="food-list-img-container">
+                  {/* Apply box shadow to the image */}
+                  <img className="food-list-img" src={item?.img} alt="img" />
+                </div>
+                <div className="food-data-item-container">
+                  <p className="food-list-dish-name">
+                    {item?.dishName.length > 20
+                      ? item?.dishName.slice(0,20) + "..."
+                      : item?.dishName}
+                  </p>
+                  <p className="food-list-dish-price">{item?.price}</p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
