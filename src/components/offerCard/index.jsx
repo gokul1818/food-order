b43e765a4f4d1912500d53cd06f782d4237a8d77
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2024-06-05 17:48:19 +0530

    offers

--- src/components/offerCard/index.jsx
+++ src/components/offerCard/index.jsx
@@ -1,35 +1,219 @@
-import React from "react";
-import "./styles.css";
+import { collection, getDocs } from "firebase/firestore";
+import { Howl } from "howler";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import clickSound from "../../assets/effect/clickSound.mp3";
+import trashSound from "../../assets/effect/trash.mp3";
 import ribbonIcon from "../../assets/images/ribbon.png";
+import { db } from "../../firebaseConfig";
+import { addCartItem, removeCartItem } from "../../redux/reducers/cartSlice";
 import NormalBtn from "../normalButton";
-export const OfferCard = ({ item, onClick }) => {
+import "./styles.css";
+
+export const OfferCard = ({ item, type = 1 }) => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
+  const isColumn = type === 1;
+
+  const [foodItems, setFoodItems] = useState([]);
+
+  const [addToCartBtnLabels, setAddToCartBtnLabels] = useState(
+    Array(foodItems.length).fill("Add to Cart")
+  );
+
+  const [itemQuantities, setItemQuantities] = useState(
+    Array(foodItems.length).fill(0)
+  );
+
+  const cartSound = new Howl({
+    src: [clickSound],
+    volume: 1,
+  });
+  const trash = new Howl({
+    src: [trashSound],
+    volume: 0.5,
+  });
+
+  const addToCart = (item, index) => {
+    cartSound.play();
+    dispatch(addCartItem({ item, quantity: 1 }));
+
+    const newLabels = [...addToCartBtnLabels];
+    newLabels[index] = "Plus";
+    setAddToCartBtnLabels(newLabels);
+
+    const newQuantities = [...itemQuantities];
+    newQuantities[index]++;
+    setItemQuantities(newQuantities);
+  };
+
+  const removeFromCart = (item, index) => {
+    trash.play();
+
+    dispatch(removeCartItem({ item, quantity: 1 }));
+    const newQuantities = [...itemQuantities];
+    newQuantities[index]--;
+    setItemQuantities(newQuantities);
+
+    if (newQuantities[index] === 0) {
+      const newLabels = [...addToCartBtnLabels];
+      newLabels[index] = "Add to Cart";
+      setAddToCartBtnLabels(newLabels);
+    }
+  };
+
+  const itemQuantity = (item) => {
+    const quantity = cart.filter((x) => x?.dishName == item?.dishName);
+    return quantity[0]?.quantity;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "categories"));
+        const items = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setFoodItems(items);
+      } catch (e) {
+        console.error("Error fetching documents: ", e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(cart);
   return (
-    <div className="body" onClick={onClick}>
-      <div className="card-view">
-        <div className="image-view">
-          <img src={item.image} alt="img" className="card-image" />
-          {item.type === "Combo" && (
-            <img src={item.image1} alt="img" className="card-image1" />
-          )}
+    <div>
+      {Boolean(isColumn) ? (
+        <div className="body">
+          <div className="card-view">
+            <div className="image-view">
+              <img src={item.image} alt="img" className="card-image" />
+              {item.type === "Combo" && (
+                <img src={item.image1} alt="img" className="card-image1" />
+              )}
+            </div>
+            <div className="ribbon-container">
+              <p className="type">{item?.type}</p>
+              <img src={ribbonIcon} alt="img" className="ribbon" />
+            </div>
+            <div className="view-details">
+              <div className="name-view">
+                <p className="name">{item?.name}</p>
+              </div>
+              <div className="btn-container">
+                {itemQuantity(item) > 0 ? (
+                  <div className="d-flex align-items-center justify-content-evenly w-100 animation-ease-in">
+                    <NormalBtn
+                      btnlabel={"-"}
+                      className={"food-increase-btn-hz"}
+                      onClick={() => removeFromCart(item)}
+                    />
+                    <span className="food-list-quantity mx-3">
+                      {itemQuantity(item)}
+                    </span>
+                    <NormalBtn
+                      btnlabel={"+"}
+                      className={"food-increase-btn-hz"}
+                      onClick={() => addToCart(item)}
+                    />
+                  </div>
+                ) : (
+                  <NormalBtn
+                    btnlabel={"Add to cart"}
+                    className={"food-list-btn"}
+                    onClick={() => addToCart(item)}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="ribbon-container">
-          <p className="type">{item?.type}</p>
-          <img src={ribbonIcon} alt="img" className="ribbon" />
+      ) : (
+        <div className="body-hz">
+          <div className="card-view-hz">
+            <div className="image-view-hz">
+              <img src={item.image} alt="img" className="card-image-hz" />
+              {item.type === "Combo" && (
+                <img src={item.image1} alt="img" className="card-image1-hz" />
+              )}
+            </div>
+
+            <div className="ribbon-container-hz">
+              <p className="type">{item?.type}</p>
+              <img src={ribbonIcon} alt="img" className="ribbon-hz" />
+            </div>
+            <p className="name-hz">{item?.name}</p>
+              {itemQuantity(item) > 0 ? (
+                <div className="d-flex align-items-center justify-content-evenly w-100 animation-ease-in">
+                  <NormalBtn
+                    btnlabel={"-"}
+                    className={"food-increase-btn-hz"}
+                    onClick={() => removeFromCart(item)}
+                  />
+                  <span className="food-list-quantity mx-3">
+                    {itemQuantity(item)}
+                  </span>
+                  <NormalBtn
+                    btnlabel={"+"}
+                    className={"food-increase-btn-hz"}
+                    onClick={() => addToCart(item)}
+                  />
+                </div>
+              ) : (
+                <NormalBtn
+                  btnlabel={"Add to cart"}
+                  className={"food-list-btn-hz"}
+                  onClick={() => addToCart(item)}
+                />
+              )}
+          </div>
         </div>
-        <div className="view-details">
-          {/* <p className="discount">{`${item?.discount} %`}</p> */}
-          <p className="name">{item?.name}</p>
-          {/* <div className="btn-container"> */}
-            <NormalBtn
-              btnlabel={"Add to cart"}
-              className={"food-list-btn"}
-              onClick={(e) => {
-                console.log(e);
-              }}
-            />
-          {/* </div> */}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
+// const [modalIsOpen, setModalIsOpen] = useState(false);
+// const [selectedOffer, setSelectedOffer] = useState(null);
+
+// const openModal = (offer) => {
+//   setSelectedOffer(offer);
+//   setModalIsOpen(true);
+// };
+
+// const closeModal = () => {
+//   setModalIsOpen(false);
+//   setSelectedOffer(null);
+// };
+
+{
+  /* <Modal show={modalIsOpen} handleClose={() => setModalIsOpen(false)}>
+        {selectedOffer && (
+          <div className="modal-body">
+            <h2 className="food-name">{selectedOffer.name}</h2>
+            <p className="food-discount">Discount: {selectedOffer.discount}%</p>
+            <p className="food-type">Type: {selectedOffer.type}</p>
+            <div className="image-container">
+              <img
+                src={selectedOffer.image}
+                alt={selectedOffer.name}
+                className="food-image"
+              />
+              {selectedOffer.type === "Combo" && (
+                <img
+                  src={selectedOffer.image1}
+                  alt={`${selectedOffer.name} combo`}
+                  className="food-image1"
+                />
+              )}
+            </div>
+
+            <button onClick={closeModal}>Continue Order</button>
+          </div>
+        )}
+      </Modal> */
+}
--- src/page/offers/index.jsx
+++ src/page/offers/index.jsx
@@ -1,67 +1,67 @@
 import React, { useState } from "react";
-import Modal from "../../components/modal/modal";
 import Navbar from "../../components/navBar/index";
 import { OfferCard } from "../../components/offerCard";
 import OfferList from "../../offers.json";
 import "./style.css";
 
 function Offers() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedOffer, setSelectedOffer] = useState(null);
+  const filterData = [
+    "special",
+    "combo",
+    "kh",
+    "lkj",
+    "bnm",
+  ];
 
-  const openModal = (offer) => {
-    setSelectedOffer(offer);
-    setModalIsOpen(true);
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  const handleFilterClick = (filter) => {
+    if (selectedFilter === filter) {
+      setSelectedFilter("");
+    } else {
+      setSelectedFilter(filter);
+    }
   };
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setSelectedOffer(null);
-  };
+  const filteredOffers = selectedFilter
+    ? OfferList.filter((offer) => offer.type.toLowerCase() === selectedFilter)
+    : OfferList;
 
   return (
-    <div className="bg-color ">
+    <div className="bg-color">
       <Navbar />
-      <div className="ease-in  ">
+      <div className="ease-in">
         <h2 className="mt-5 pt-5 d-flex flex-column justify-content-center align-items-center">
           Today's Offers
         </h2>
-
-        {OfferList.map((offer) => (
-          <OfferCard
-            key={offer.id}
-            item={offer}
-            // onClick={() => openModal(offer)}
-          />
-        ))}
-        <Modal show={modalIsOpen} handleClose={() => setModalIsOpen(false)}>
-          {selectedOffer && (
-            <div>
-              <h2 className="food-name">{selectedOffer.name}</h2>
-              <p className="food-discount">
-                Discount: {selectedOffer.discount}%
-              </p>
-              <p className="food-type">Type: {selectedOffer.type}</p>
-              <div className="image-container">
-                <img
-                  src={selectedOffer.image}
-                  alt={selectedOffer.name}
-                  className="food-image"
-                />
-                {selectedOffer.type === "Combo" && (
-                  <img
-                    src={selectedOffer.image1}
-                    alt={`${selectedOffer.name} combo`}
-                    className="food-image1"
-                  />
+        <div className="horizontal-view">
+          {filterData.map((filter, index) => (
+            <div
+              key={index}
+              className={`filter-view ${
+                selectedFilter === filter ? "selected" : ""
+              }`}
+              onClick={() => handleFilterClick(filter)}
+            >
+              <p className="filter-style">
+                {filter}
+                {selectedFilter === filter && (
+                  <span className="cancel-icon">×</span>
                 )}
-              </div>
-
-              <button onClick={closeModal}>Close</button>
-              <button onClick={closeModal}>Continue Order</button>
+              </p>
             </div>
-          )}
-        </Modal>
+          ))}
+        </div>
+        <div className="horizontal-view">
+          {filteredOffers.map((offer) => (
+            <OfferCard key={offer.id} item={offer} type={2} />
+          ))}
+        </div>
+        <div className="offer-cards">
+          {filteredOffers.map((offer) => (
+            <OfferCard key={offer.id} item={offer} type={1} />
+          ))}
+        </div>
       </div>
     </div>
   );
